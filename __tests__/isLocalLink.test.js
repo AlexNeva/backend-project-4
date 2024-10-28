6f5c7e24db713414b156f4643d19e17583a09d22
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2024-10-28 22:44:53 +0300

    Add test

--- __tests__/isLocalLink.test.js
+++ __tests__/isLocalLink.test.js
@@ -3,6 +3,11 @@ import { isLocalLink } from '../src/utils/isLocalLink.js';
 const resources = [
   { link: '/assets/application.css', baseUrl: 'https://example.com', expected: true },
   { link: 'https://ru.example.com', baseUrl: 'https://example.com', expected: false },
+  {
+    link: 'https://ru.hexlet.io/packs/js/runtime.js',
+    baseUrl: 'https://ru.hexlet.io',
+    expected: true,
+  },
 ];
 
 test.each(resources)('isLocalLink($link, $baseUrl)', ({ link, baseUrl, expected }) => {
