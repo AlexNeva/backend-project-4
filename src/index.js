2d70d517bb13fb5976a6c184acf2dbd077bd6b51
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2025-03-11 22:28:11 +0300

    Add debuggers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import { downloadHtml } from './loaders/htmlLoader.js';
 import { resolve } from 'node:path';
 import { downloadResources } from './loaders/resourcesLoader.js';
+import debug from 'debug';
+
+const log = debug('page-loader');
 
 const pageLoader = (url, dir = process.cwd()) => {
   const dirPath = resolve(dir);
+
+  log('Загрузка страницы');
 
   downloadHtml(url, dir)
     .then((pathToHtml) => downloadResources(pathToHtml, url))
